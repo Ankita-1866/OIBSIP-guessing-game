70b16a01ad1b4279b13484d374149495d1dc687e
Ankita-1866 <117670509+Ankita-1866@users.noreply.github.com>
2022-12-24 16:46:50 +0530

    Add files via upload

---
+++ taskk2.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+public class taskk2 {
+  public static void main(String[] args) {
+    Random Random_number= new Random();
+    int right_guess=Random_number.nextInt(100);
+    int turns=0;
+    Scanner scan=new Scanner(System.in);
+    System.out.println("Guess no. between 1 to 100" );
+    System.out.println("u will get 10 turns");
+    
+    int guess;
+    int i=0;
+    boolean win=false;
+    while(win==false) {
+      guess=scan.nextInt();
+      turns++;
+    
+    if(guess==right_guess) {
+      win=true;
+    }
+    else if(i>8){
+      System.out.println("You loose! the right answer was: "+right_guess);
+      return;
+    }
+    else if(guess<right_guess){
+      i++;
+      System.out.println("Yor Guess is lower than the right guess! Turns left: "+(10-i));
+      
+      
+      
+    }
+    else if(guess>right_guess) {
+      i++;
+      System.out.println("Your Guess Is Higher Than THe Right Guess! Turns left: "+(10-i));
+      
+    }
+    
+    
+  }
+    System.out.println("YOu win!");
+    System.out.println("Then number was "+right_guess);
+    System.out.println("You used "+turns+" turns to guess the right number");
+    System.out.println("Your score is "+((11-turns)*10)+" out of 100");
+    
+}
+}
